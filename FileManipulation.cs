7fc53d16f98288b49f9f79ce3b70a561b0d36eef
SDIV30 <106191688+SDIV30@users.noreply.github.com>
2024-06-19 16:45:14 +0300

    Add project files.

---
+++ FileManipulation.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+using System.Drawing;
+using Image =System.Drawing.Image;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using Path = System.IO.Path;
+
+namespace ImageDataCleaner
+{
+    internal class FileManipulation
+    {
+        internal static List<string> imagePaths= new List<string>();
+        internal static string pickedFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        internal List<string> getImages() {
+            using (CommonOpenFileDialog fileDialog = new CommonOpenFileDialog()) {           
+                fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                fileDialog.Multiselect = true;
+                if(fileDialog.ShowDialog()== CommonFileDialogResult.Ok)
+                {
+                    imagePaths.Clear();
+                    imagePaths= fileDialog.FileNames.ToList();
+                }
+
+                List<string> fileNames = new List<string>();
+                for (int i = 0; i < imagePaths.Count(); i++)
+                {
+                    fileNames.Add(Path.GetFileName(imagePaths[i]));
+                }
+                return fileNames;
+            }
+        }
+        internal string getFolder()
+        {
+            using (CommonOpenFileDialog fileDialog = new CommonOpenFileDialog())
+            {
+                fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                fileDialog.IsFolderPicker = true;
+                if (fileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    DirectoryInfo dirInf = new DirectoryInfo(fileDialog.FileName);
+                    if(dirInf.Attributes!=FileAttributes.ReadOnly)
+                    {
+                        pickedFolder = fileDialog.FileName;
+                    }
+                }
+                    return pickedFolder;
+            }
+        }
+        
+        internal void cleanMeta(string fileName, string path, string prefix) 
+        {
+            using (Image img = Image.FromFile(fileName))
+            {
+                var allPoprItem = img.PropertyItems;
+                const int description = 0x10e;
+                const int comment = 0x9286;
+                const int software = 0x0131;
+                
+                using (Bitmap imgCopy = new Bitmap(img)) 
+                {
+                    if (imgCopy.PropertyItems.Any(x => x.Id == description))
+                    {
+                        imgCopy.RemovePropertyItem(description);
+                    }
+
+                    if (imgCopy.PropertyItems.Any(x => x.Id == comment))
+                    {
+                        imgCopy.RemovePropertyItem(comment);
+                    }
+
+                    if (imgCopy.PropertyItems.Any(x => x.Id == software))
+                    {
+                        imgCopy.RemovePropertyItem(software);
+                    }
+                    //string directory = Path.GetDirectoryName(path);
+                    //string directory = path;
+                    string prefixedFileName = $"{prefix}{Path.GetFileName(fileName)}";
+                    imgCopy.Save(Path.Combine(path, prefixedFileName), ImageFormat.Png);
+                }
+            }
+        }
+
+        internal void runFiles (string newPath, string prefix)
+        {
+            if (imagePaths.Count != 0) { 
+                List<string> imgs = imagePaths;
+                if (newPath == "")
+                {
+                    newPath = Path.GetDirectoryName(imgs[0]);
+                }
+
+                if (prefix == "") 
+                { 
+                    prefix = "Clr_";
+                }
+
+                for (int i = 0; i < imgs.Count(); i++)
+                {
+                    cleanMeta(imgs[i], newPath, prefix);
+                }
+                imagePaths.Clear();
+                pickedFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageDataCleaner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileManipulation fileM;
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileM = new FileManipulation();
+        }
+        private void cleanData(object sender, RoutedEventArgs e) {
+            string folder = "";
+            if (checkCopy.IsChecked == true)
+            {
+                folder = folderText.Text;
+            }
+            fileM.runFiles(folder, prefixText.Text);
+            filesText.Clear();
+            prefixText.Clear();
+            folderText.Clear();
+            //MessageBox.Show("Description and comentary data deleted","Operation successful",MessageBoxButton.OK);
+        }
+
+        private void selectFolder(object sender, RoutedEventArgs e)
+        {
+            folderText.Text =fileM.getFolder();
+
+        }
+        private void selectFiles(object sender, RoutedEventArgs e)
+        {
+            List<string> fileNames=fileM.getImages();
+            filesText.Text = String.Join(";", fileNames);
+
+        }
+    }
+}
